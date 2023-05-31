c97dcadeae53ad5ee15023844be4beafb878465c
nguyenhuuphuc.cse <nguyenhuuphuc.cse@gmail.com>
2023-06-01 03:46:44 +0700

    use .env for secret

--- db/config.js
+++ db/config.js
@@ -1,7 +1,10 @@
+require("dotenv").config()
+
 function getConnectionString() {
-  const password = "ahihidongoc"
-  const conn_string = `nguyenhuuphuc:${password}@initialcluster.ib8vkmc.mongodb.net/url-shortener?retryWrites=true&w=majority`
+  let { DB_USER, DB_PASS } = process.env
+  const conn_string = `${DB_USER}:${DB_PASS}@initialcluster.ib8vkmc.mongodb.net/url-shortener?retryWrites=true&w=majority`
   const uri = `mongodb+srv://${conn_string}`
+  console.log(uri)
   return uri
 }
 
